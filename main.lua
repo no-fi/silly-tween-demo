f0688ee532aa2c1983af4b7616d04a010e41ea1c
no-fi <no-fi@users.noreply.github.com>
2016-05-10 23:28:08 -0500

    added transparency and 2 spins

--- main.lua
+++ main.lua
@@ -1,8 +1,8 @@
 local tween = require('tween')
 local ghost = nil
-local gd = { sx = 1, sy = 1, r = 0 }
+local gd = { sx = 1, sy = 1, r = 0, a = 0 }
 local r = 0
-local t = tween.new(3, gd, { sx = 10, sy = 10, r = math.pi * 2}, 'inBounce')
+local t = tween.new(3, gd, { sx = 15, sy = 15, r = math.pi * 4, a = 255}, 'inBounce')
 
 function love.load()
   love.graphics.setDefaultFilter('nearest', 'nearest', 1)
@@ -23,5 +23,6 @@ end
 
 function love.draw()
   love.graphics.translate(love.graphics.getWidth()/2, love.graphics.getHeight()/2)
+  love.graphics.setColor(255, 255, 255, gd.a)
   love.graphics.draw(ghost, 0, 0, gd.r, gd.sx, gd.sy, ghost:getWidth()/2, ghost:getHeight()/2)
 end
